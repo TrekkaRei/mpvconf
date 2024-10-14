5748e551c515e0f4f14fb8cfc23bfbf83b689cba
TrekkaRei <TrekkaRei@gmail.com>
2024-10-14 18:25:25 +0200

    update

--- scripts/mpvrun0.sh
+++ scripts/mpvrun0.sh
@@ -1,2 +1,2 @@
-gnome-terminal --geometry=58x4+1020+50 -t "mpv player" -- mpv --screen=0 --geometry=1920x1080+320+250 --loop /home/trekka/Videos/ffmpeg/rec0.mkv
-xprop -name 'mpv player' -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x2, 0x0, 0x0"
+gnome-terminal --geometry=58x4+1020+50 -t "mpv info" -- mpv --screen=0 --geometry=1920x1080+313+222 --loop /home/trekka/Videos/ffmpeg/rec0.mkv
+xprop -name 'mpv info' -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x2, 0x0, 0x0" && sleep 1 && xprop -name 'MPV player' -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x2, 0x0, 0x0"
